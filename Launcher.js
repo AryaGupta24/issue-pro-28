c0dfdd9e5feb3f1bb85c0799c3462882346ea784
Arya Gupta <71432307+AryaGupta24@users.noreply.github.com>
2021-01-25 16:26:32 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,23 @@
+class launcher{
+    constructor(bodyA, point2){
+        var options = {
+            bodyA: bodyA,
+            pointB: point2,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.launch = Constraint.create(options);
+        World.add(world, this.launch);
+    }
+    fly(){
+       this.launch.bodyA = null; 
+    }
+    display(){
+        if(this.launch.bodyA){
+            var anchorA = this.launch.bodyA.position;
+            var anchorB = this.launch.pointB;
+            strokeWeight(4);
+            line(anchorA.x, anchorA.y, anchorB.x, anchorB.y);
+        }  
+    }
+}
